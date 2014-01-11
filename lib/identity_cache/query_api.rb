a66a54d33d7cfc86d742b70f73a331fd197aaecf
fkruta <francois.kruta@gmail.com>
2014-01-11 12:57:04 +0100

    Update query_api.rb

--- lib/identity_cache/query_api.rb
+++ lib/identity_cache/query_api.rb
@@ -33,6 +33,8 @@ module IdentityCache
 
           require_if_necessary do
             object = nil
+            puts rails_cache_key(id).class
+            puts rails_cache_key(id)
             coder = IdentityCache.fetch(rails_cache_key(id)){ coder_from_record(object = resolve_cache_miss(id)) }
             object ||= record_from_coder(coder)
             IdentityCache.logger.error "[IDC id mismatch] fetch_by_id_requested=#{id} fetch_by_id_got=#{object.id} for #{object.inspect[(0..100)]} " if object && object.id != id.to_i
